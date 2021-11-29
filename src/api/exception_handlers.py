3f792eda173928f3ea5fec1005b3635e215d3cb9
Javad <javad.amin@outlook.com>
2021-11-29 09:07:37 +0100

    Fixing a problem where status code was always 200 for all responses.

--- src/api/exception_handlers.py
+++ src/api/exception_handlers.py
@@ -13,25 +13,25 @@ def handle_exceptions(app):
     def handle_already_exists_database_exception(e):
         return create_exception_response(HTTPStatus.BAD_REQUEST.value,
                                          str(e),
-                                         HTTPStatus.BAD_REQUEST.name)
+                                         HTTPStatus.BAD_REQUEST.name), HTTPStatus.BAD_REQUEST.value
 
     @app.errorhandler(AlreadyClaimedDatabaseException)
     def handle_already_claimed_database_exception(e):
         return create_exception_response(HTTPStatus.BAD_REQUEST.value,
                                          str(e),
-                                         HTTPStatus.BAD_REQUEST.name)
+                                         HTTPStatus.BAD_REQUEST.name), HTTPStatus.BAD_REQUEST.value
 
     @app.errorhandler(NotFoundDatabaseException)
     def handle_not_found_database_exception(e):
         return create_exception_response(HTTPStatus.NOT_FOUND.value,
                                          str(e),
-                                         HTTPStatus.NOT_FOUND.name)
+                                         HTTPStatus.NOT_FOUND.name), HTTPStatus.NOT_FOUND.value
 
     @app.errorhandler(Exception)
     def handle_exception(e):
         return create_exception_response(HTTPStatus.INTERNAL_SERVER_ERROR.value,
                                          f"Internal server error. {str(e)}",
-                                         HTTPStatus.INTERNAL_SERVER_ERROR.name)
+                                         HTTPStatus.INTERNAL_SERVER_ERROR.name), HTTPStatus.INTERNAL_SERVER_ERROR.value
 
 
 def create_exception_response(status_code, message, status_name):
